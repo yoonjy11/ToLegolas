72068ace699f5f9d26fc549e32f08181f49e14dc
7y9j3 <7y9j3@gagagaga>
2022-12-06 00:37:49 +0900

    docs : upgrade

--- againLegolas/src/LegolasYJY/LegolasYJY/Company.java
+++ againLegolas/src/LegolasYJY/LegolasYJY/Company.java
@@ -1,16 +1,17 @@
-package LegolasYJY;
+package LegolasYJY.LegolasYJY;
 
 public class Company
 {
-	private String name;
+	private String name, bigo;
 	private int cash;
 	
 	
-	public Company(String name, int cash)
+	public Company(String name, int cash, String bigo)
 	{
 		super();
 		this.name = name;
 		this.cash = cash;
+		this.bigo = bigo;
 	}
 
 
@@ -22,6 +23,10 @@ public class Company
 	{
 		return cash;
 	}
+	public String getBigo()
+	{
+		return bigo;
+	}
 	public void setName(String name)
 	{
 		this.name = name;
@@ -29,6 +34,10 @@ public class Company
 	public void setCash(int cash)
 	{
 		this.cash = cash;
+	}
+	public void setBigo(String bigo)
+	{
+		this.bigo = bigo;
 	}
 	
 }
--- againLegolas/src/LegolasYJY/LegolasYJY/Run.java
+++ againLegolas/src/LegolasYJY/LegolasYJY/Run.java
@@ -1,5 +1,5 @@
 
-package LegolasYJY;
+package LegolasYJY.LegolasYJY;
 
 import java.util.Scanner;
 
@@ -11,33 +11,52 @@ public class Run
 		Merge mg = new Merge();
 		
 
+		mg.MergeFunction();
+		
 		mg.intro();
+		
+		mg.compBuy();
+		
+		int tek = mg.compBuy();
 		String mycomp = mg.intro();
-		while(true) 
+		if (tek>10) {mg.soldout(tek);};
+		if(tek<=11)
 		{
-			mg.compBuy();
-			String buy = mg.compBuy();
-
-			switch(buy) 
-			{ 
-			case "깃허브" : case "피그마" : case "슬랙":  case "ARM" : case "블리자드":
-				System.out.println("실 패");
-				System.out.println("살 수 없습니다.");
-				System.out.println("이미 다른 회사에게 인수되었습니다.");
-				mg.soldout(buy); 
-				break;
-//			case  : case  :case  :case  :case  :case  :
-				//				System.out.println("실 패");
-				//				System.out.println("자금이 부족합니다");
-				////				  OOO 사는_ _ _ 원  (당신의 돈)
-				////				  좀더 싼 회사를 찾아보세요 }
-				//				break;
-			default :
-				System.out.println("성 공");
-				System.out.printf("당신은 오늘부터 %s%s의 사장입니다.", buy, myCompany);
-				break;
+			for(int i = 0; i<=11; i++) 
+			{
+				if(mycomp == mg.comp[i].getName()) 
+				{
+					System.out.println(mg.comp[i]);
+				}
 			}
-		}
+		};
+		
+//		while(true) 
+//		{
+//			mg.compBuy();
+//			 buy = mg.compBuy();
+//
+//			switch(buy) 
+//			{ 
+//			case "깃허브" : case "피그마" : case "슬랙":  case "ARM" : case "블리자드":
+//				System.out.println("실 패");
+//				System.out.println("살 수 없습니다.");
+//				System.out.println("이미 다른 회사에게 인수되었습니다.");
+//				mg.soldout(buy); 
+//				break;
+////			case  : case  :case  :case  :case  :case  :
+//				//				System.out.println("실 패");
+//				//				System.out.println("자금이 부족합니다");
+//				////				  OOO 사는_ _ _ 원  (당신의 돈)
+//				////				  좀더 싼 회사를 찾아보세요 }
+//				//				break;
+//			default :
+//				System.out.println("성 공");
+////				System.out.printf("당신은 오늘부터 %s%s의 사장입니다.", buy, myCompany);
+//				break;
+//			}
+//		}
+		mg.ending();
 	}
 }
 
